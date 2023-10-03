d6050c8446ce2256e3f53cb0f5bf27ed2a7af0b4
Carolina Souza <carolinasouza@tce.ma.gov.br>
2023-10-03 13:00:06 -0300

    organização dos pacotes

---
+++ src/main/java/com/slzcitycruiser/basepackage/package-info.java
@@ -0,0 +1,9 @@
+package com.slzcitycruiser.basepackage;
+
+/*
+ * Use o conteúdo deste pacote para montar uma estrutura vazia
+ * moldar funcionalidades relacionada a uma entidade usando a
+ * arquitetura hexagonal.
+ *
+ * @Author Carolina Souza
+ */
