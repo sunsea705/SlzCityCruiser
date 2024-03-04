b029619530861dc5a18e2b76da8a094c035ca4d8
Carolina Souza <carolinasouza@tce.ma.gov.br>
2024-03-04 10:59:50 -0300

    ok

---
+++ src/main/java/com/sunsea/slzcitycruiser/SlzcitycruiserApplication.java
@@ -0,0 +1,13 @@
+package com.sunsea.slzcitycruiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlzcitycruiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlzcitycruiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunsea/slzcitycruiser/SlzcitycruiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunsea.slzcitycruiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlzcitycruiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
